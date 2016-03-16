8b3314767690870d8049262b53be77821d7a0705
Greg Pechiro <gregpechiro@yahoo.com>
2016-03-16 12:08:06 -0400

    improved server side script for lauching project

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# a script to remove old files and folders
-# this should only be run on the server where the project lives right before
-# redeployment. It will remove all files that are contained in the new .tar
-
-DIR=`echo ${PWD##*/}`
-
-rm -rf static/ templates/ *.go ${DIR} ${DIR}.tar
