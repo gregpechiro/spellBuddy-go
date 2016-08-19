194802b7897a6334ec88958d8ec2fcfb6a34e7ba
Greg Pechiro <gregpechiro@yahoo.com>
2016-08-19 14:54:05 -0400

    fixed visual when searching

--- static/js/search.js
+++ static/js/search.js
@@ -4,8 +4,13 @@ function search() {
         if (spells.length > 0) {
             spellSet = spells.filter(filterSearch);
             spellSet.sort(orderSpell);
+            $('span.letter').removeClass('label-primary');
+            $('span.letter').addClass('label-default');
         }
     } else {
+        var span = $('span[data-let="' + letter + '"]');
+        span.removeClass('label-default');
+        span.addClass('label-primary');
         setSpellSet();
     }
     genResults(paginate(spellSet));
